43d6461c71b0196b299967f8f5478912a9160916
Arsen Musayelyan <moussaelianarsen@gmail.com>
2022-11-30 21:43:52 -0800

    Move pkgPrompt() to cli.go

---
+++ cli.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/AlecAivazis/survey/v2"
+	"go.arsenm.dev/lure/internal/db"
+)
+
+func pkgPrompt(options []db.Package, verb string) ([]db.Package, error) {
+	names := make([]string, len(options))
+	for i, option := range options {
+		names[i] = option.Repository + "/" + option.Name + " " + option.Version
+	}
+
+	prompt := &survey.MultiSelect{
+		Options: names,
+		Message: "Choose which package(s) to " + verb,
+	}
+
+	var choices []int
+	err := survey.AskOne(prompt, &choices)
+	if err != nil {
+		return nil, err
+	}
+
+	out := make([]db.Package, len(choices))
+	for i, choiceIndex := range choices {
+		out[i] = options[choiceIndex]
+	}
+
+	return out, nil
+}
--- repo.go
+++ repo.go
@@ -113,28 +113,3 @@ func refreshCmd(c *cli.Context) error {
 	}
 	return nil
 }
-
-func pkgPrompt(options []db.Package, verb string) ([]db.Package, error) {
-	names := make([]string, len(options))
-	for i, option := range options {
-		names[i] = option.Repository + "/" + option.Name + " " + option.Version
-	}
-
-	prompt := &survey.MultiSelect{
-		Options: names,
-		Message: "Choose which package(s) to " + verb,
-	}
-
-	var choices []int
-	err := survey.AskOne(prompt, &choices)
-	if err != nil {
-		return nil, err
-	}
-
-	out := make([]db.Package, len(choices))
-	for i, choiceIndex := range choices {
-		out[i] = options[choiceIndex]
-	}
-
-	return out, nil
-}
