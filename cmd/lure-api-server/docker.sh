9aa9aa4072fa4760abed43d643d56b00b652ce54
Elara Musayelyan <elara@elara.ws>
2022-12-21 11:37:30 -0800

    Disable docker build cache

--- cmd/lure-api-server/docker.sh
+++ cmd/lure-api-server/docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build
-docker buildx build --platform linux/amd64 --tag arsen6331/lure-api-server:amd64 .
+docker buildx build --platform linux/amd64 --tag arsen6331/lure-api-server:amd64 --no-cache .
 
 CGO_ENABLED=0 GOARCH=arm64 GOOS=linux go build
-docker buildx build --platform linux/arm64/v8 --tag arsen6331/lure-api-server:arm64 .
+docker buildx build --platform linux/arm64/v8 --tag arsen6331/lure-api-server:arm64 --no-cache .
 
 docker login
 docker push arsen6331/lure-api-server -a
