73bdb5449652b08cb964040253774e272ccc6faf
Arsen Musayelyan <arsen@arsenm.dev>
2023-01-30 23:12:29 -0800

    Add missing GPL headers and change year to 2023

--- repo.go
+++ repo.go
@@ -1,6 +1,6 @@
 /*
  * LURE - Linux User REpository
- * Copyright (C) 2022 Arsen Musayelyan
+ * Copyright (C) 2023 Arsen Musayelyan
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
